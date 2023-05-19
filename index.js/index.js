bb5eb8ee3aa681ec4f4803b67fe61ad5a9fd6bf6
julien blondel <julien.blondel218@yahoo.com>
2023-05-19 23:33:53 +0200

    first save 19.05

---
+++ index.js/index.js
@@ -0,0 +1,42 @@
+// Header reaction on click
+let preview = null
+const headerWrapper = Array.from(document.querySelectorAll('.headerWrapper ul li'))
+console.log(headerWrapper);
+
+let clickLink = function (event) {
+    event.preventDefault()
+    event.target.classList.add('linkClicked')
+    if (preview !== null) {
+        preview.classList.remove('linkClicked');
+    }
+    preview = event.target
+}
+headerWrapper.forEach((el) => {
+    el.addEventListener('click', clickLink)
+})
+
+// scroll delete header
+const justheaderWrapper = document.querySelector('.headerWrapper')
+window.addEventListener('scroll', function () {
+
+    if (window.scrollY > 200) {
+        justheaderWrapper.classList.add('hidden')
+    } else {
+        justheaderWrapper.classList.remove('hidden')
+
+    }
+
+})
+
+// hover contact
+const iconContact = document.querySelector('.fa-envelope-open-text')
+const contactLinkJs = document.querySelector('.contactLinkJs')
+contactLinkJs.classList.add('unVisibility')
+
+console.log(iconContact);
+iconContact.addEventListener('mouseenter', function () {
+    contactLinkJs.classList.remove('unVisibility')
+
+    contactLinkJs.classList.add('Visibility')
+})
+
